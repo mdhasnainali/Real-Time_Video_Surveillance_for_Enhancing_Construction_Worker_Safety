be7e4b0014fbf540bbdb6e20fcb7c4a7304c38bd
mdhasnainali <mdhasnainali.01@gmail.com>
2024-11-02 08:29:36 +0600

    Modify evaluate.py and data.yaml

--- evaluate.py
+++ evaluate.py
@@ -2,17 +2,11 @@ import argparse
 from ultralytics import YOLO
 
 def evaluate(model_name):
-    model = YOLO(f'./Gun_Detection/{model_name}/weights/best')
-    results = model.val(data='data.yaml')
+    model = YOLO(f'./Gun_Detection/{model_name}/weights/best.pt')
+    results = model.val(data='data.yaml', split='test', imgsz=640, batch=16, device=0, project='Gun_Detection', name=f'{model_name}_Validation', patience=30, plots=True) 
 
-    # Retrieve mAP metrics and other details
-    map50 = results.box.map50  # mAP@0.5
-    map5095 = results.box.map  # mAP@0.5:0.95
-    precision = results.box.precision.mean()
-    recall = results.box.recall.mean()
-    f1_score = results.box.f1.mean()
-
-    print(f'Model: {model_name}, mAP50: {map50:.4f}, mAP50-95: {map5095:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1_score:.4f}')
+    print(f'Results of {model_name}: {results.results_dict}')
+    print(f'Speed of {model_name}:{results.speed}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Evaluate YOLO Models for Object Detection")
